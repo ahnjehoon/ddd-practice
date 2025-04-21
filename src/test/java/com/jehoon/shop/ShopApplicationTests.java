8b97a96c8944b63913d626a31c40290732d42c43
ahnjehoon <ahnjehoon@naver.com>
2025-04-21 23:25:08 +0900

    init

---
+++ src/test/java/com/jehoon/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jehoon.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
