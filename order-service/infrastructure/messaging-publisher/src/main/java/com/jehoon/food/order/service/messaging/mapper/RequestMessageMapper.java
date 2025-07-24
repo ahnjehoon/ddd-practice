334ca44f51bc9ec859e3e68831f6ad9f62f2e861
ahnjehoon <ahnjehoon@naver.com>
2025-07-24 22:20:21 +0900

    refact(order): 1차 리팩토링

---
+++ order-service/infrastructure/messaging-publisher/src/main/java/com/jehoon/food/order/service/messaging/mapper/RequestMessageMapper.java
@@ -0,0 +1,67 @@
+package com.jehoon.food.order.service.messaging.mapper;
+
+import com.jehoon.food.kafka.model.order.avro.PaymentOrderStatus;
+import com.jehoon.food.kafka.model.order.avro.PaymentRequestAvroModel;
+import com.jehoon.food.kafka.model.order.avro.RestaurantApprovalRequestAvroModel;
+import com.jehoon.food.order.service.domain.entity.Order;
+import com.jehoon.food.order.service.domain.event.OrderCancelledEvent;
+import com.jehoon.food.order.service.domain.event.OrderCreatedEvent;
+import com.jehoon.food.order.service.domain.event.OrderPaidEvent;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Component
+public class RequestMessageMapper {
+
+    public PaymentRequestAvroModel orderCreatedEventToPaymentRequestAvroModel(OrderCreatedEvent orderCreatedEvent) {
+        Order order = orderCreatedEvent.getOrder();
+        return PaymentRequestAvroModel.newBuilder()
+                .setId(UUID.randomUUID())
+                .setSagaId(UUID.randomUUID())
+                .setCustomerId(order.getCustomerId().getValue())
+                .setOrderId(order.getId().getValue())
+                .setPrice(order.getPrice().getAmount())
+                .setCreatedAt(orderCreatedEvent.getCreatedAt().toInstant())
+                .setPaymentOrderStatus(PaymentOrderStatus.PENDING)
+                .build();
+    }
+
+    public PaymentRequestAvroModel orderCancelledEventToPaymentRequestAvroModel(
+            OrderCancelledEvent orderCancelledEvent) {
+        Order order = orderCancelledEvent.getOrder();
+        return PaymentRequestAvroModel.newBuilder()
+                .setId(UUID.randomUUID())
+                .setSagaId(UUID.randomUUID())
+                .setCustomerId(order.getCustomerId().getValue())
+                .setOrderId(order.getId().getValue())
+                .setPrice(order.getPrice().getAmount())
+                .setCreatedAt(orderCancelledEvent.getCreatedAt().toInstant())
+                .setPaymentOrderStatus(PaymentOrderStatus.CANCELLED)
+                .build();
+    }
+
+    public RestaurantApprovalRequestAvroModel orderPaidEventToRestaurantApprovalRequestAvroModel(
+            OrderPaidEvent orderPaidEvent) {
+        Order order = orderPaidEvent.getOrder();
+        return RestaurantApprovalRequestAvroModel.newBuilder()
+                .setId(UUID.randomUUID())
+                .setSagaId(UUID.randomUUID())
+                .setOrderId(order.getId().getValue())
+                .setRestaurantId(order.getRestaurantId().getValue())
+                .setRestaurantOrderStatus(com.jehoon.food.kafka.model.order.avro.RestaurantOrderStatus
+                        .valueOf(order.getOrderStatus().name()))
+                .setProducts(order.getOrderItems().stream()
+                        .map(orderItem -> com.jehoon.food.kafka.model.order.avro.Product
+                                .newBuilder()
+                                .setId(orderItem.getProduct().getId().getValue()
+                                        .toString())
+                                .setQuantity(orderItem.getQuantity())
+                                .build())
+                        .collect(Collectors.toList()))
+                .setPrice(order.getPrice().getAmount())
+                .setCreatedAt(orderPaidEvent.getCreatedAt().toInstant())
+                .build();
+    }
+}
---
+++ order-service/infrastructure/messaging-subscriber/src/main/java/com/jehoon/food/order/service/messaging/mapper/ResponseMessageMapper.java
@@ -0,0 +1,43 @@
+package com.jehoon.food.order.service.messaging.mapper;
+
+import com.jehoon.food.kafka.model.order.avro.PaymentResponseAvroModel;
+import com.jehoon.food.kafka.model.order.avro.RestaurantApprovalResponseAvroModel;
+import com.jehoon.food.order.service.domain.dto.message.PaymentResponse;
+import com.jehoon.food.order.service.domain.dto.message.RestaurantApprovalResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ResponseMessageMapper {
+
+    public PaymentResponse paymentResponseAvroModelToPaymentResponse(
+            PaymentResponseAvroModel paymentResponseAvroModel) {
+        return PaymentResponse.builder()
+                .id(paymentResponseAvroModel.getId())
+                .sagaId(paymentResponseAvroModel.getSagaId())
+                .paymentId(paymentResponseAvroModel.getPaymentId())
+                .customerId(paymentResponseAvroModel.getCustomerId())
+                .orderId(paymentResponseAvroModel.getOrderId())
+                .price(paymentResponseAvroModel.getPrice())
+                .createdAt(paymentResponseAvroModel.getCreatedAt())
+                .paymentStatus(com.jehoon.food.domain.valueobject.PaymentStatus.valueOf(
+                        paymentResponseAvroModel.getPaymentStatus().name()))
+                .failureMessages(paymentResponseAvroModel.getFailureMessages())
+                .build();
+    }
+
+    public RestaurantApprovalResponse approvalResponseAvroModelToApprovalResponse(
+            RestaurantApprovalResponseAvroModel restaurantApprovalResponseAvroModel) {
+        return RestaurantApprovalResponse.builder()
+                .id(restaurantApprovalResponseAvroModel.getId())
+                .sagaId(restaurantApprovalResponseAvroModel.getSagaId())
+                .restaurantId(restaurantApprovalResponseAvroModel.getRestaurantId())
+                .orderId(restaurantApprovalResponseAvroModel.getOrderId())
+                .createdAt(restaurantApprovalResponseAvroModel.getCreatedAt())
+                .orderApprovalStatus(
+                        com.jehoon.food.domain.valueobject.OrderApprovalStatus.valueOf(
+                                restaurantApprovalResponseAvroModel
+                                        .getOrderApprovalStatus().name()))
+                .failureMessages(restaurantApprovalResponseAvroModel.getFailureMessages())
+                .build();
+    }
+}
